60678dace04fd22c9be39a932b09499eb42eb037
AUPUN523 <Supunkawihara9@gmail.com>
2025-01-07 12:33:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '923444844060';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '+94741800523';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.pairingNumber = process.env.BOT_NUMBER || 'your number';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '+94741809523';  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'OQgWxLzY#J0tq92Ioyb2PDgxzKWp9-OhmQcteYa7eGDGffJuPmJ4';  // put your session id here
 
 global.mods = []
 global.prems = []
