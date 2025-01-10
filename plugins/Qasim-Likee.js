5b0be529f64298cffe176bd351d9634788c79599
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-10 16:38:30 +0530

    Update Qasim-Likee.js

--- plugins/Qasim-Likee.js
+++ plugins/Qasim-Likee.js
@@ -63,7 +63,7 @@ const handler = async (m, { conn, args }) => {
     const fileName = mediaData.data.title ? `${mediaData.data.title}.mp4` : 'media.mp4';
     const mimetype = 'video/mp4';
 
-    await conn.sendFile(m.chat, mediaBuffer, fileName, `*𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 © 𝚄𝙻𝚃𝚁𝙰-𝙼𝙳*`, m, false, { mimetype });
+    await conn.sendFile(m.chat, mediaBuffer, fileName, `*POWERD BY © DEW-XMD*`, m, false, { mimetype });
     m.react('✅');
   } catch (error) {
     console.error('Error downloading from Likee:', error.message, error.stack);
