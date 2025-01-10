1b4d33028bb94416c83e19189233a3a8587c4a1d
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-10 17:21:15 +0530

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
   let vcard = `BEGIN:VCARD
 VERSION:3.0
-N:;Qasim;;;
-FN:Qasim Ali
-ORG:GlobalTechInfo
+N:;Hansa;;;
+FN:Hansa Dewmina
+ORG:Dew-Coders
 TITLE:Owner
-item1.TEL;waid=923444844060:923444844060
+item1.TEL;waid=94701515609:94778721239
 item1.X-ABLabel:Owner
 X-WA-BIZ-DESCRIPTION:Developer of the Bot
-X-WA-BIZ-NAME:Qasim Ali
+X-WA-BIZ-NAME:Hansa Dewmina
 END:VCARD`;
 
   await conn.sendMessage(m.chat, {
     contacts: {
-      displayName: 'Qasim Ali',
+      displayName: 'Hansa Dewmina',
       contacts: [{ vcard }]
     }
   }, { quoted: m });
