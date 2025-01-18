95984788a73c79d97a4b00c5a8155a428abc08db
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 14:46:42 +0530

    Update tool-autosticker.js

--- plugins/tool-autosticker.js
+++ plugins/tool-autosticker.js
@@ -11,7 +11,7 @@ handler.all = async function (m) {
   if (chat.autosticker && m.isGroup) {
     let q = m
     let stiker = false
-    let wm = 'GlobalTech'
+    let wm = 'DewCoders'
     let mime = (q.msg || q).mimetype || q.mediaType || ''
     if (/webp/g.test(mime)) return
     if (/image/g.test(mime)) {
