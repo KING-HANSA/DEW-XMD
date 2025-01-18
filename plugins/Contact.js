ae1ef4565f524ef0ff7a35c2737027cc921fd988
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 11:33:49 +0530

    Update and rename New.js to Contact.js

--- plugins/Contact.js
+++ plugins/Contact.js
@@ -6,12 +6,12 @@ let handler = async m =>
     *අප හා සම්බන්ධ වීමට මෙම පියවර අනුගමනය කරන්න* 
 
   1. *Join This Channel Link First After This My Owner Will Answer You*
-  > *https://whatsapp.com/channel/0029Varwcfc7YSd60AulNF2h*
+  >*https://whatsapp.com/channel/0029Varwcfc7YSd60AulNF2h*
 
   __________________________
 
   2. *If This Is Not Real Owner Please Contact The Devoloper*
-  > *94701515609* *©HansaDewmina*
+  >*94701515609* *©HansaDewmina*
   __________________________
 
 
