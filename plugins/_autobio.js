04027f65740a6bf25dbf863e0a49c502210cc832
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 11:45:49 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ handler.all = async function (m) {
         })) * 1000
     }
     let muptime = clockString(_muptime)
-    let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By Hansa`
+    let bio = `\n🟢 BOT Time Active ${muptime}\n\n ┃ 💎 BY DEW-XMD`
     await this.updateProfileStatus(bio).catch(_ => _)
     setting.status = new Date() * 1
   }
