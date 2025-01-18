1ef6742970fa61538ab2c5a3921d3caf20abb0e5
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 22:07:16 +0530

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     global.db.data.chats[m.chat].sBye = text;
     m.reply(`✅ Goodbye message updated successfully!`);
   } else {
-    throw `✳️ Goodbye message සදහා යමක් ඇතුලත් කරන්න.`;
+    throw `✳️ Please provide a new goodbye message.`;
   }
 }
 
