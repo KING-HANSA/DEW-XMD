192ab107e475efdd0854e20f85f3aa049950e15b
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 21:58:27 +0530

    Update _AutoVoice.js

--- plugins/_AutoVoice.js
+++ plugins/_AutoVoice.js
@@ -1,11 +1,8 @@
-
 let handler = m => m
 handler.all = async function (m) {
   for (const message in audioMsg) {
     if (new RegExp(`^${message}$`, 'i').test(m.text)) {
-      this.sendFile(m.chat, audioMsg[message], 'audio.mp3') , null, m, false, {
-            mimetype: 'audio/mpeg'
-        });
+      this.sendFile(m.chat, audioMsg[message], 'audio.mp3', null, m, true)
       break
     }
   }
@@ -14,9 +11,8 @@ handler.all = async function (m) {
 
 export default handler
 
-
 let audioMsg = {
-  'pakaya': 'https://d.top4top.io/m_330525jip1.mp3',
+  'pakaya':'https://d.top4top.io/m_330525jip1.mp3',
   'gm':'https://i.top4top.io/m_3305mqb1j1.mp3',
   'kawada':'https://j.top4top.io/m_330520o4j2.mp3',
   'mk':'https://k.top4top.io/m_3305geq693.mp3',
