7ee1e7ee3e3ac618d3cad97784db895b6871a118
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 21:35:48 +0530

    Update _AutoVoice.js

--- plugins/_AutoVoice.js
+++ plugins/_AutoVoice.js
@@ -15,4 +15,8 @@ export default handler
 
 let audioMsg = {
   'pakaya': 'https://d.top4top.io/m_330525jip1.mp3',
+  'gm':'https://i.top4top.io/m_3305mqb1j1.mp3',
+  'kawada':'https://j.top4top.io/m_330520o4j2.mp3',
+  'mk':'https://k.top4top.io/m_3305geq693.mp3',
+  'gn':'https://l.top4top.io/m_3305ef1254.mp3'
 }
