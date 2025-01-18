11ca86a54e704d0a4b58c45fa2fba3f7e16d4896
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 17:28:50 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,20 +5,9 @@ import { createHash } from 'crypto';
 import { xpRange } from '../lib/levelling.js';
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let d = new Date(new Date() + 3600000);
-    let locale = 'en';
-    let week = d.toLocaleDateString(locale, { weekday: 'long' });
-    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' });
-    let _uptime = process.uptime() * 1000;
-    let uptime = clockString(_uptime);
 
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`;
-
-    let user = global.db.data.users[who];
-    let { level } = user;
-    let { min, xp, max } = xpRange(level, global.multiplier);
-    let greeting = ucapan();
 
     let str = `
       『 *MEGA-AI* 』  
@@ -56,29 +45,4 @@ handler.help = ['main'];
 handler.tags = ['group'];
 handler.command = ['menu2', 'help2', 'h', 'commands2'];
 
-export default handler;
-
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':');
-}
-
-function ucapan() {
-    const time = moment.tz('Asia/Karachi').format('HH');
-    let res = "happy early in the day☀️";
-    if (time >= 4) {
-        res = "Good Morning 🥱";
-    }
-    if (time >= 10) {
-        res = "Good Afternoon 🫠";
-    }
-    if (time >= 15) {
-        res = "Good Afternoon 🌇";
-    }
-    if (time >= 18) {
-        res = "Good Night 🌙";
-    }
-    return res;
-}
+export default handler
