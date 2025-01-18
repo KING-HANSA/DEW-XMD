6bceda3c4e5628e573643236e54a99f7322dce1a
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-18 11:53:38 +0530

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -5,19 +5,19 @@ let handler = m => m
 handler.all = async function (m) {
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg')
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/YXp71EV.jpeg')
 	
 	//reply link wa
-   global.rpgc = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', mediaType: 'VIDEO', description: 'support group', title: 'JOIN GROUP', body: 'support group', thumbnailUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07' }}} 
+   global.rpgc = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.imgur.com/YXp71EV.jpeg', mediaType: 'VIDEO', description: 'support group', title: 'JOIN GROUP', body: 'support group', thumbnailUrl: 'https://i.imgur.com/YXp71EV.jpeg', sourceUrl: 'https://whatsapp.com/channel/0029Varwcfc7YSd60AulNF2h' }}} 
 	
 	//reply link Instagram 
-    global.rpig = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', mediaType: 'VIDEO', description: 'FOLLOW DEVELOPER', title: 'INSTAGRAM', body: 'Keep bot alive', thumbnailUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', sourceUrl: 'https://instagram.com/global.techinfo' }}}
+    global.rpig = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.imgur.com/YXp71EV.jpeg', mediaType: 'VIDEO', description: 'FOLLOW DEVELOPER', title: 'INSTAGRAM', body: 'Keep bot alive', thumbnailUrl: 'https://i.imgur.com/YXp71EV.jpeg', sourceUrl: 'https://www.instagram.com/hansa_dewmina_lk' }}}
 	
 	//reply link yt
-    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', mediaType: 'VIDEO', description: 'SUBSCRIBE : YT CHANNEL', title: 'YouTube', body: 'learn to create your own bots', thumbnailUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', sourceUrl: 'https://youtube.com/@GlobalTechInfo' }}}
+    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.imgur.com/YXp71EV.jpeg', mediaType: 'VIDEO', description: 'SUBSCRIBE : YT CHANNEL', title: 'YouTube', body: 'learn to create your own bots', thumbnailUrl: 'https://i.imgur.com/YXp71EV.jpeg', sourceUrl: 'https://www.youtube.com/@KingHansa' }}}
 
 //reply link WhatsApp Channel
-    global.rpwp = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', mediaType: 'VIDEO', description: 'Follow Channel', title: 'FOLLOW CHANNEL', body: '© GlobalTechInfo', thumbnailUrl: 'https://i.ibb.co/9HY4wjz/a4c0b1af253197d4837ff6760d5b81c0.jpg', sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07' }}}
+    global.rpwp = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.imgur.com/YXp71EV.jpeg', mediaType: 'VIDEO', description: 'Follow Channel', title: 'FOLLOW CHANNEL', body: '© DEW-XMD', thumbnailUrl: 'https://i.imgur.com/YXp71EV.jpeg', sourceUrl: 'https://whatsapp.com/channel/0029Varwcfc7YSd60AulNF2h' }}}
     
 } 
 export default handler
